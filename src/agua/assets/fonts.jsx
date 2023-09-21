85b4bdef690a83a34495738a6cd45578f3b34d97
david-dom <113909984+david-dom@users.noreply.github.com>
2023-09-21 11:34:14 -0500

    My commit message

---
+++ src/agua/assets/fonts.jsx
@@ -0,0 +1,15 @@
+import { Roboto } from 'next/font/google';
+import localFont from "next/font/local";
+                
+export const GoogleRoboto = Roboto({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: '400',
+    variable: '--Roboto'
+});
+export const _apple_system,_system_ui,_BlinkMacSystemFont,_'Roboto',_sans_serif = localFont({
+    src: "./fonts/-apple-system,_system-ui, BlinkMacSystemFont, 'Roboto', sans-serif.undefined",
+    display: 'swap',
+    variable: "--_apple_system,_system_ui,_BlinkMacSystemFont,_'Roboto',_sans_serif",
+});
+
